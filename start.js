c2b6dc0821135e8faa698ace06999ae53d099d53
Okabe0511 <okabe05@outlook.com>
2025-03-03 21:02:42 +0800

    Add files via upload

---
+++ start.js
@@ -0,0 +1,10 @@
+function check(){
+    pwd = document.getElementById("pwd").value;
+    if (pwd == '1145'){
+        window.location.href='https://okabe0511.github.io/okabeweb.github.io/'
+    }
+    else {
+        alert('false'); 
+    }
+
+}
